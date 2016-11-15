77acd32a4acb34b25875267e7644ea7d6287a0d3
Evan Yand <evanyand@gmail.com>
2016-11-15 11:25:21 -0800

    working draft

--- KL.py
+++ KL.py
@@ -4,28 +4,84 @@
 K-L Algorithm
 ECE428
 Evan Yand
-Step 1
+Final
 '''
 
-import argparse
 import os
 import re
+import copy
 
 
 
 def main():
 
         KLgraph = Graph()
-        dir = "/home/evan/dd"# input("Enter file path: ")
+        
+        dir = input("Enter directory of input file (press enter for default): ")
+        
+        if dir == '':
+            dir = "/home/evan/kl"# input("Enter file path: ")
+        
+        
         f = open(dir, 'r')
         readin(f, KLgraph)
         
         KLgraph.findcost()
-        KLgraph.gains()
+        startcost = KLgraph.Extcost
 
-        #KLgraph.split(part1)
+        stop = False
 
-        KLgraph.outputgraph()
+        while(stop == False):
+            
+            KLiter = []
+            KLiter.append(KLgraph)
+            for i in range(int(KLgraph.Nvertices/2)):
+                if i == 0 :
+                    
+                    KLiter[i].findcost()
+                    (v1, v2, g) = KLiter[i].gains()
+                    
+                    print("Cost: " + str(KLgraph.Extcost))
+                else:              
+                    KLiter.append(copy.deepcopy(KLiter[i-1]))
+                    KLiter[i].swap(v1, v2)
+                    
+                    KLiter[i].findcost()
+                    
+                    (v1, v2, g) = KLiter[i].gains()
+                #print(i)
+            
+            iterMax = 0
+            maxIndex = 0
+
+            j = 0
+            iterTotal = 0
+            for graph in KLiter:
+                iterTotal += graph.Maxgain
+                
+                if iterTotal > iterMax:
+                    maxIndex = j
+                    iterMax = iterTotal
+                j += 1
+
+            print()
+            print("Taking graph " + str(maxIndex))
+            
+            if(maxIndex == 0):
+                print("orig:")
+                print(startcost)
+                KLgraph.outputgraph()
+                stop = True
+
+            else:
+                KLgraph = Graph()
+                KLgraph.Part1 = KLiter[maxIndex].Part1
+                KLgraph.Part2 = KLiter[maxIndex].Part2
+                KLgraph.Nedges = KLiter[maxIndex].Nedges
+                KLgraph.Nvertices = KLiter[maxIndex].Nvertices
+                KLgraph.iteration = KLiter[maxIndex].iteration + 1  
+                print("Iteration " + str(KLgraph.iteration))
+        #KLgraph.outputgraph()
 
 
 def readin(file, graph):
@@ -81,7 +137,9 @@ class Graph(object):
                 self.Gains1 = []
                 self.Gains2 = []
                 self.Gains = []
-
+                
+                self.Locked = []
+                self.Maxgain = 0
                 self.iteration = 0
 
         def findcost(self):
@@ -132,16 +190,29 @@ class Graph(object):
 
         def gains(self):
             i = 0
+            self.Gains = []
+            self.Gains1 = []
+            self.Gains2 = []
 
-            for ver in self.Part1.keys():
-                for con in self.Part2.keys():
-                    self.Gains1.append(ver)
-                    self.Gains2.append(con)
-                    self.Gains.append(self.Dcosts[ver] + self.Dcosts[con] - 2)
+            for ver in sorted(self.Part1.keys()):
+                if ver in self.Locked:
+                    pass
+                else:
+                    for con in sorted(self.Part2.keys()):
+                        if con in self.Locked:
+                           pass 
+                        else:
+                            self.Gains1.append(ver)
+                            self.Gains2.append(con)
+                            if int(con) in self.Part1[ver]:
+                                self.Gains.append(self.Dcosts[ver] + self.Dcosts[con] - 2)
+                                #print(str(ver) + " & " + str(con) + " are connected")
+                            else:
+                                self.Gains.append(self.Dcosts[ver] + self.Dcosts[con])
+                                #print(str(ver) + " & " + str(con) + " are not connected")
+                            #print("The gain of " + str(ver) + " and " + str(con) + " is " + str(self.Gains[i]))
 
-                    #print("The gain of " + str(ver) + " and " + str(con) + " is " + str(self.Gains[i]))
-
-                    i += 1
+                            i += 1
             j = 0
             highest = self.Gains[0]
             selection = 0
@@ -151,7 +222,23 @@ class Graph(object):
                     selection = j
                 j += 1
             
-            print("The (first) largest possible gain is: " + str(highest) + " by swapping " + str(self.Gains1[selection]) + " and " + str(self.Gains2[selection]))
+           # print("The (first) largest possible gain is: " + str(highest) + " by swapping " + str(self.Gains1[selection]) + " and " + str(self.Gains2[selection]))
+            
+            #swap(self.Gains1[selection], self.Gains2[selection])
+            
+            self.Maxgain = self.Gains[selection]
+
+            return((self.Gains1[selection], self.Gains2[selection], self.Gains[selection]))
+
+        def swap(self, v1, v2):
+            self.Part2[v1] = self.Part1[v1]
+            self.Part1.pop(v1)
+
+            self.Part1[v2] = self.Part2[v2]
+            self.Part2.pop(v2)
+            
+            self.Locked.append(v1)
+            self.Locked.append(v2)
 
 
 
@@ -165,12 +252,12 @@ class Graph(object):
                 print("Iteration: " + str(self.iteration))
                 print()
                 print("PARTITION 1")
-                for ver in self.Part1.keys():
+                for ver in sorted(self.Part1.keys()):
                         print(ver)
 
                 print()
                 print("PARTITION 2")
-                for ver in self.Part2.keys():
+                for ver in sorted(self.Part2.keys()):
                         print(ver)
 
                 print()
